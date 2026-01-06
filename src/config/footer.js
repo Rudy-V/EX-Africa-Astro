39e9682f3d9fee81c4ca0355c5aa860b908c239a
Rudolph <rudolph.a.visagie@gmail.com>
2026-01-06 06:55:47 +0200

    Created home and about page

--- src/config/footer.js
+++ src/config/footer.js
@@ -1,50 +1,24 @@
 export const footerSocials = [
-  {
-    name: 'Twitter',
-    url: 'https://twitter.com/jaydanurwin',
-    icon: "mdi:twitter",
-  },
-  {
-    name: 'Youtube',
-    url: 'https://youtube.com/@jaydanurwin',
-    icon: "mdi:youtube",
-  },
-  {
-    name: 'Github',
-    url: 'https://github.com/jaydanurwin',
-    icon: "mdi:github",
-  },
-  {
-    name: 'Discord',
-    url: 'https://astro.build/chat',
-    icon: "ic:baseline-discord",
-  },
-]
+	{
+		name: 'Youtube',
+		url: '',
+		icon: 'mdi:youtube',
+	},
+	{
+		name: 'Instagram',
+		url: '',
+		icon: 'mdi:instagram',
+	},
+
+];
 
 export const footerLists = [
-	{
-		title: 'Landing Pages',
-		items: [
-			{
-				title: 'Landing Page 1',
-				slug: '/landing-pages/landing-1',
-			},
-			{
-				title: 'Landing Page 2',
-				slug: '/landing-pages/landing-2',
-			},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		],
-	},
 	{
 		title: 'Company',
 		items: [
 			{
 				title: 'About',
-				slug: '/company/about',
+				slug: '/about',
 			},
 			{
 				title: 'Blog',
@@ -52,7 +26,7 @@ export const footerLists = [
 			},
 			{
 				title: 'Contact',
-				slug: '/company/contact',
+				slug: '/contact',
 			},
 		],
 	},
@@ -67,14 +41,22 @@ export const footerLists = [
 				title: 'Style Guide',
 				slug: '/theme/style-guide',
 			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
+			{
+				title: 'Theme Setup',
+				slug: '/theme/theme-setup',
+			},
+			{
+				title: 'Customizing Odyssey',
+				slug: '/theme/customizing-odyssey',
+			},
+			{
+				title: 'Landing Page 1',
+				slug: '/landing-pages/landing-1',
+			},
+			{
+				title: 'Landing Page 2',
+				slug: '/landing-pages/landing-2',
+			},
 		],
 	},
 ];
