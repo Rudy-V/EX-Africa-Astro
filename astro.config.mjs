4d2b44377de71d650a33e0e94e70647f2a25ad86
Rudolph <rudolph.a.visagie@gmail.com>
2026-04-12 14:02:44 +0200

    Test payment links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import lit from "@astrojs/lit";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static', // Ensure fully static output for Vercel free tier
-  site: 'https://odyssey-theme.sapling.supply/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  output: 'static',
+  adapter: vercel(),
+  site: 'https://experienceafrica.co.za', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
 });
